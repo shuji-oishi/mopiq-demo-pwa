d979b16464850a80bb293b97a60f2d2dbab9aedc
shuji_oishi <shuji_oishi@migiteq.com>
2025-02-12 17:54:23 +0900

    update

--- pages/index.js
+++ pages/index.js
@@ -14,7 +14,7 @@ export default function Home() {
     if (typeof window !== 'undefined' && 'serviceWorker' in navigator) {
       window.addEventListener('load', () => {
         navigator.serviceWorker
-          .register('/service-worker.js')
+          .register('/sw.js')
           .then((registration) => {
             console.log('Service Worker registered:', registration);
           })
