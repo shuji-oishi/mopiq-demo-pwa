41d303575867eced4d077f932b64359f538ce4cd
shuji_oishi <shuji_oishi@migiteq.com>
2025-02-12 18:13:29 +0900

    更新

--- next.config.js
+++ next.config.js
@@ -1,12 +1,28 @@
 /** @type {import('next').NextConfig} */
 const withPWA = require('next-pwa')({
   dest: 'public',
-  disable: process.env.NODE_ENV === 'development'
+  disable: process.env.NODE_ENV === 'development',
+  register: true,
+  skipWaiting: true,
+  sw: '/service-worker.js'
 })
 
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true
+  swcMinify: true,
+  headers: async () => {
+    return [
+      {
+        source: '/service-worker.js',
+        headers: [
+          {
+            key: 'Service-Worker-Allowed',
+            value: '/'
+          }
+        ]
+      }
+    ]
+  }
 }
 
 module.exports = withPWA(nextConfig)
